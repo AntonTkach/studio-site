83fabcec0d1ca8c05d3b839b2a3de8f91803ed0a
Anton Tkach <dekerter@gmail.com>
2018-01-02 01:48:50 +0200

    Fixed grid layout. Loader speeds set

--- loading/main.js
+++ loading/main.js
@@ -1,1 +1,24 @@
 // JavaScript source code
+function gebi(id) {
+    return document.getElementById(id);
+}
+
+function StartLoading() {
+    //for (;;) {
+        
+    //}
+    var elem = document.getElementById("loadingBar");
+    var width = 0;
+    var speed = 0.2; // times per second 0.01-5
+    var id = setInterval(frame, 10/2);
+    function frame() {
+        if (width >= 100) {
+            //clearInterval(id);
+            width = 0;
+        } else {
+            width += speed/2;
+            elem.style.width = width + '%';
+        }
+    }
+    
+}
