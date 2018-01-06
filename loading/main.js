3e1029506595028d498296d1afc3174b1be3d14b
Anton Tkach <dekerter@gmail.com>
2018-01-07 00:23:05 +0200

    Initial data and functions set

--- loading/main.js
+++ loading/main.js
@@ -2,23 +2,62 @@
 function gebi(id) {
     return document.getElementById(id);
 }
-
+var gameData = {
+    points: 0,
+    isMainLoaderSold: false,
+    isMainLoadBarSold: false,
+    isPercentSold: false,
+    isMenuSold: false,
+    isTabsSold: false,
+    isTimeSold:false,
+    isSaveSold: false,
+    isToggablesSold: false,
+    isManualbuySold: false
+    //barSpeeds: [1, 0.5], // seconds per one run
+    //currentBarSpeed: 0.1 
+};
+//var barSpeed = 0.1; // times per second 0.01-5
+var barSpeed = 2;// seconds per one run
 function StartLoading() {
-    //for (;;) {
-        
-    //}
-    var elem = document.getElementById("loadingBar");
+    var elem = gebi("loadingBar");
     var width = 0;
-    var speed = 0.2; // times per second 0.01-5
-    var id = setInterval(frame, 10/2);
+    
+    //var id = setInterval(frame, 10/2);
     function frame() {
         if (width >= 100) {
             //clearInterval(id);
             width = 0;
+            gameData.points++;
+            
         } else {
-            width += speed/2;
-            elem.style.width = width + '%';
+            width += 1/barSpeed/2;
+            elem.style.width = width +"%";
         }
     }
+    Purchase(gameData.points, gameData.isManualbuySold);
     
+}
+
+function Purchase(points, mode) {
+    if (mode===false) {
+        BuyNext();
+    }
+}
+
+function BuyNext() {
+    for (var key in gameData) {
+        if (gameData.hasOwnProperty(key) === false) {
+            gameData[key] = true;
+        }
+    }
+    // Loader, loading bar, loading percent, tabs, save, 
+    return;
+}
+
+function EnableStuff() {
+    for (var key in gameData) {
+        if (gameData.hasOwnProperty(key) === true) {
+            gebi(/*element*/).style.display = "block";
+        }
+    }
 }
